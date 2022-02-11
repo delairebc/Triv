5163f7a5cd86995b7c6d154f38cdc5e2f51bbe4f
Ant3ney <46980442+Ant3ney@users.noreply.github.com>
2022-02-10 23:06:17 -0800

    Add more info to readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Triv
-An API designed to facilitate Trivia testing knowledge.
+
+An API designed to facilitate an exam-like test.
+
+## Future Road Map
+
+Utilize ML models to check how well multi line text answers match answers questions.
+
+## Setup
+
+Place TestSheet.java in a directory where the class can be instantiated.
--- TestSheet.java
+++ TestSheet.java
@@ -1,18 +1,26 @@
-
+//Each test will be its own object and will facilitate its own logic
 public class TestSheet {
-	private String[] questions;
-	private int[] answers;
-	private int currentIndex = 0;
-	
-	 public TestSheet(String[] questions, int[] answers, String[][] options) {
-		this.questions = questions;
-		this.answers = answers;
-	}
-	
-	public String getQuestion(int index) {
-		return this.questions[index];
-	}
-	public Boolean gradeQuestion(int question, int choice) {
-		return this.answers[question] == choice;
-	}
+    private String[] questions;
+    private int[] answers;
+    private int currentIndex = 0;
+   
+    //Pass in an array of questions, answers, and a two dimensional array for the options
+     public TestSheet(String[] questions, int[] answers, String[][] options) {
+        this.questions = questions;
+        this.answers = answers;
+    }
+   
+    //Get the question based of index from argument
+    public String getQuestion(int index) {
+        return this.questions[index];
+    }
+ 
+    //Determine whether or not a choice is correct.
+    /**
+    * @param: question: the index of the question to grade
+    * @param: choice: the index of the choice to grade
+     */
+    public Boolean gradeQuestion(int question, int choice) {
+        return this.answers[question] == choice;
+    }
 }
